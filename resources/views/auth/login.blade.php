251a69eac9d5c4421516ae71144d522ca0af5695
ianfjrd <ianfajardo0930@gmail.com>
2023-04-19 18:39:58 +0800

    Login validation fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
                                 </div>
 
                                 <h5 class="text-xl font-medium text-white dark:text-white text-center">Fill up form below</h5>
-                                <x-validation-errors class="mb-4 border" />
+                                <x-validation-errors/>
                                 <div>
                                     <label for="email" class="block mb-2 text-sm font-medium text-white dark:text-white" value="{{ __('Email') }}">E-mail</label>
                                     <input type="email" name="email" :value="old('email')" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required>
