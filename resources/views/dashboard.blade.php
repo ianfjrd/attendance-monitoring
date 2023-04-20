0024e9ae0a43fc50eba383115e291d39ab284f48
johnperdigon <johnpatrick.perdigon.l@bulsu.edu.ph>
2023-04-20 14:19:31 +0800

    Minor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
 
  <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-        <a class="flex items-center pl-2.5 mb-5">
+        <a class="flex items-center pl-2.5 mb-5 mt-1">
            <img src="{{asset('images/logo.png')}}" class="h-6 mr-3 sm:h-7" alt="Marvill Logo" />
         </a>
        <ul class="space-y-2 font-medium">
