5c15da0c83e0cc2c2d80b6743375a0a2d33ee6c2
ianfjrd <ianfajardo0930@gmail.com>
2023-04-26 18:51:02 +0800

    Add User Post Draft
    Continue tom

--- resources/views/admin/employee/admin-addUser.blade.php
+++ resources/views/admin/employee/admin-addUser.blade.php
@@ -1,43 +1,51 @@
 @extends('admin.admindashboard')
 
 @section('content')
-    <div class="flex justify-center items-center">
-        <label for="title" class="text-4xl font-bold dark:text-white pt-5 pb-10">Add Employee Form</label>
-    </div>
+    <form action="POST" action="{{ route('register') }}">
+        @csrf
 
-
-
-    <div class="grid grid-cols-2">
-        <div class="flex flex-col justify-center items-center text-center">
-            <label class=" text-[#8EC33F] font-bold text-2xl">Name</label>
-            <input type="text" name="floating_first_name" id="floating_first_name" placeholder="John Doe"
-                class=" text-center block py-2.5 px-0 w-1/2 text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer"
-                placeholder=" " required />
+        <div class="flex justify-center items-center">
+            <label for="title" class="text-4xl font-bold dark:text-white pt-5 pb-10">Add Employee Form</label>
         </div>
 
-        <div class="flex flex-col justify-center items-center text-center">
-            <label class=" text-[#8EC33F] font-bold text-2xl">Email</label>
-            <input type="email" name="floating_first_name" id="floating_first_name" placeholder="sample@sample.com"
-                class=" text-center block py-2.5 px-0 w-1/2 text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer"
-                placeholder=" " required />
-        </div>
-    </div>
+        <div class="grid grid-cols-2 py-5">
+            <div class="flex flex-col justify-center items-center text-center">
+                <label class=" text-[#8EC33F] font-bold text-2xl">Name</label>
+                <x-input id="name"
+                    class=" text-center block py-2.5 px-0 w-1/2 text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer"
+                    type="text" name="name" :value="old('name')" required autofocus autocomplete="name"
+                    placeholder="John Doe" />
+            </div>
 
-    <div class="grid grid-cols-2">
-        <div class="flex flex-col text-center items-center justify-center">
-            <label class="text-[#8EC33F] font-bold text-2xl">Break In</label>
-            <input type="time"
-                class="border-0 border-b-2 dark:text-white bg-transparent text-2xl w-1/4 px-0 block py-2.5 text-gray-900 appearance-none border-gray-300 dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer" />
+            <div class="flex flex-col justify-center items-center text-center">
+                <label class=" text-[#8EC33F] font-bold text-2xl">Email</label>
+                <x-input id="email"
+                    class=" text-center block py-2.5 px-0 w-1/2 text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer"
+                    type="email" name="email" :value="old('email')" required autocomplete="username"
+                    placeholder="sample@sample.com" />
+            </div>
         </div>
 
-        <div class="flex flex-col text-center items-center justify-center">
-            <label class="text-[#8EC33F] font-bold text-2xl">Break Out</label>
-            <input type="time"
-                class="border-0 border-b-2 dark:text-white bg-transparent text-2xl w-1/4 px-0 block py-2.5 text-gray-900 appearance-none border-gray-300 dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer" />
-        </div>
-    </div>
+        <div class="grid grid-cols-2 py-5">
+            <div class="flex flex-col justify-center items-center text-center">
+                <label class=" text-[#8EC33F] font-bold text-2xl">Password</label>
 
-    <div class="flex justify-center items-center py-10">
-        <x-button>Add Form</x-button>
-    </div>
+                <x-input id="password"
+                    class=" text-center block py-2.5 px-0 w-1/2 text-1xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer"
+                    type="password" name="password" required autocomplete="new-password"
+                    placeholder="Must have atleast 6 characters" />
+            </div>
+
+            <div class="flex flex-col justify-center items-center text-center">
+                <label class=" text-[#8EC33F] font-bold text-2xl">Age</label>
+                <input type="number" name="age"
+                    class=" text-center block py-2.5 px-0 w-1/4 text-2xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-[#8EC33F] dark:focus:border-[#8EC33F] focus:outline-none focus:ring-0 focus:border-[#8EC33F] peer"
+                    placeholder="21" required />
+            </div>
+        </div>
+
+        <div class="flex justify-center items-center py-10">
+            <x-button>{{ __('Register') }}</x-button>
+        </div>
+    </form>
 @endsection
