ce223de19a5e578a36e653503061835f26a4990c
Justine-12345 <castanedajustine09@gmail.com>
2023-05-01 13:17:08 +0800

    reduce modal background opacity

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,14 @@
             <ul class="space-y-2 font-medium">
                 <li>
                     <a href="{{ route('dashboard') }}"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        class=" flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                         <svg aria-hidden="true"
                             class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                             <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
                         </svg>
-                        <span class="ml-3">Dashboard</span>
+                        <span class="ml-3  ">Dashboard</span>
                     </a>
                 </li>
                 <li>
@@ -132,8 +132,8 @@
 
 
     <div class="p-4 sm:ml-64 my-auto relative  ">
-       
-       
+
+
 
         <div class="p-4  dark:text-white ">
             <!-- Main modal -->
@@ -212,6 +212,27 @@
         </div>
     </div>
     <script type="text/javascript">
+        // set the modal menu element
+        const $targetEl = document.getElementById('authentication-modal');
+
+        // options with default values
+        const options = {
+            placement: 'bottom-right',
+            backdrop: 'dynamic',
+            backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+            closable: true,
+            onHide: () => {
+                console.log('modal is hidden');
+            },
+            onShow: () => {
+                console.log('modal is shown');
+            },
+            onToggle: () => {
+                console.log('modal has been toggled');
+            }
+        };
+        const modal = new Modal($targetEl, options);
+
         setInterval(displayclock, 500);
 
         function displayclock() {
